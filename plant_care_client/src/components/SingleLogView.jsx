0a5f7db78bb9545df1afc3f1ed5fb5cd91f1467d
Stephanie Quinn Strickland <strickquinn@gmail.com>
2020-12-16 22:42:36 -0500

    create singlelogview.jsx file

---
+++ plant_care_client/src/components/SingleLogView.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleLogView extends Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
